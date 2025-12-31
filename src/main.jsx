30532a4b30d35b28d8eb31b12476818abea3aa45
kondasanireddy <kondasanireddyprasanna@gmail.com>
2025-12-31 13:13:53 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <App />
-  </HashRouter>
+  </StrictMode>
 );
