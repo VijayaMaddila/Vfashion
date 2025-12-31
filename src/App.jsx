79ebab9a6c504fc1839f8b52a60fda70c0aab659
kondasanireddy <kondasanireddyprasanna@gmail.com>
2025-12-31 13:04:48 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Component/NavBar";
 import Home from "./Component/HomePage";
 import Login from "./Component/Login";
@@ -68,7 +68,7 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar cartCount={cartCount} />
 
       <Routes>
@@ -135,7 +135,7 @@ function App() {
 
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
